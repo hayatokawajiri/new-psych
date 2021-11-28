6232945e161987a0bd127adc0c7e4b0034c82bd1
hayatokawajiri <95098394+hayatokawajiri@users.noreply.github.com>
2021-11-29 05:37:54 +0700

    add note

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -59,6 +59,7 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'GF Sing',
 		'No Animation'
+		'Heal Note'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
